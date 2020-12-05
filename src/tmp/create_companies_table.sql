c53258ba2a556d17bed1a6ac747d7599dc537aa8
Miyagi-de-engineer <t.hayato15@gmail.com>
2020-12-05 11:43:46 +0900

    composerを用いた環境変数の設定を行いました
    phpの実行でSQLの実行やDB接続ができるようになりました。

--- src/tmp/create_companies_table.sql
+++ src/tmp/create_companies_table.sql
@@ -5,3 +5,43 @@ CREATE TABLE companies (
     founder VARCHAR(255),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) DEFAULT CHARACTER SET=utf8mb4;
+
+
+CREATE TABLE memos(
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    category VARCHAR(255),
+    importance VARCHAR(100),
+    summary TEXT(1000),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+) DEFAULT CHARACTER SET=utf8mb4;
+
+INSERT INTO memos(
+    title,
+    category,
+    importance,
+    summary
+) VALUES (
+    'tomorrow',
+    'work',
+    4,
+    'document'
+);
+
+DELETE FROM memos WHERE id = '1';
+
+ALTER TABLE memos
+ADD title VARCHAR(255) AFTER id;
+
+INSERT INTO reviews(
+    title,
+    author,
+    status,
+    score,
+    summary
+) VALUES (
+    'Disney',
+    'Wolt',
+    'complete',
+    100,
+    'specialDay'
+);
