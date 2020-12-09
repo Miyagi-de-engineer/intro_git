facb36e27c9f133c851e45cebd09578f2d9bc759
Miyagi-de-engineer <t.hayato15@gmail.com>
2020-12-09 22:45:28 +0900

    HTMLのレイアウト部分を分割しました
    外部ファイルとして設け、修正の際の工数を減らす事ができました。
    同じレイアウトを用いる事で、統一感のあるサイト表現をする事ができました。

---
+++ src/views/index.php
@@ -0,0 +1,12 @@
+<a href="new.php">読書ログを登録する</a>
+<main>
+    <section class="">
+        <div class="">
+            <h2>新世界より</h2>
+            <div>貴志祐介/読了/５点</div>
+            <p>
+                寝食を忘れ、むさぼるように読んでしまった。圧倒的な神本。感情を揺さぶられる。
+            </p>
+        </div>
+    </section>
+</main>
--- src/views/new.php
+++ src/views/new.php
@@ -1,58 +1,44 @@
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>読書ログの登録</title>
-</head>
-
-<body>
-    <h1>読書ログ</h1>
-    <h2>読書ログの登録</h2>
-    <form action="create.php" method="post">
-        <?php if (count($errors)) : ?>
-            <ul>
-                <?php foreach ($errors as $error) : ?>
-                    <li><?php echo $error; ?></li>
-                <?php endforeach; ?>
-            </ul>
-        <?php endif; ?>
-        <div>
-            <label for="title">書籍名</label>
-            <input type="text" name="title" id="title" value="<?php echo $review['title']; ?>">
-        </div>
-        <div>
-            <label for="author">著者名</label>
-            <input type="text" name="author" id="author" value="<?php echo $review['author']; ?>">
-        </div>
-        <div>
-            <label>読書状況</label>
-            <div>
-                <div>
-                    <input type="radio" name="status" id="status1" value="未読" checked <?php echo ($review['status'] === '未読') ? 'checked' : ''; ?>>
-                    <label for="status1">未読</label>
-                </div>
-                <div>
-                    <input type="radio" name="status" id="status2" value="読んでる" <?php echo ($review['status'] === '読んでる') ? 'checked' : ''; ?>>
-                    <label for="status2">読んでる</label>
-                </div>
-                <div>
-                    <input class="form-check-input" type="radio" name="status" id="status3" value="読了" <?php echo ($review['status'] === '読了') ? 'checked' : ''; ?>>
-                    <label for="status3">読了</label>
-                </div>
+<h2 class="h3 text-dark mb-4">読書ログの登録</h2>
+<form action="create.php" method="post">
+    <?php if (count($errors)) : ?>
+        <ul class="text-danger">
+            <?php foreach ($errors as $error) : ?>
+                <li><?php echo $error; ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+    <div class="form-group">
+        <label for="title">書籍名</label>
+        <input type="text" class="form-control" name="title" id="title" value="<?php echo $review['title']; ?>">
+    </div>
+    <div class="form-group">
+        <label for="author">著者名</label>
+        <input type="text" name="author" class="form-control" id="author" value="<?php echo $review['author']; ?>">
+    </div>
+    <div>
+        <label>読書状況</label>
+        <div class="form-group">
+            <div class="form-check form-check-inline">
+                <input type="radio" name="status" class="form-check-input" id="status1" value="未読" checked <?php echo ($review['status'] === '未読') ? 'checked' : ''; ?>>
+                <label for="status1" class="form-check-label">未読</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input type="radio" name="status" class="form-check-input" id="status2" value="読んでる" <?php echo ($review['status'] === '読んでる') ? 'checked' : ''; ?>>
+                <label for="status2" class="form-check-label">読んでる</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input type="radio" name="status" class="form-check-input" id="status3" value="読了" <?php echo ($review['status'] === '読了') ? 'checked' : ''; ?>>
+                <label for="status3" class="form-check-label">読了</label>
             </div>
         </div>
-        <div>
-            <label for="score">評価（5点満点の整数）</label>
-            <input type="number" name="score" id="score" value="<?php echo $review['score']; ?>">
-        </div>
-        <div>
-            <label for="summary">感想</label>
-            <textarea type="text" name="summary" id="summary" rows="10"><?php echo $review['summary']; ?></textarea>
-        </div>
-        <button type="submit">登録する</button>
-    </form>
-</body>
-
-</html>
+    </div>
+    <div class="form-group">
+        <label for="score">評価（5点満点の整数）</label>
+        <input type="number" name="score" class="form-control" id="score" value="<?php echo $review['score']; ?>">
+    </div>
+    <div class="form-group">
+        <label for="summary">感想</label>
+        <textarea type="text" name="summary" class="form-control" id="summary" rows="10"><?php echo $review['summary']; ?></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">登録する</button>
+</form>
