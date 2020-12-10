eb3e7005fa3b1b0a744a08c3dd9b3f62dd23dbcc
Miyagi-de-engineer <t.hayato15@gmail.com>
2020-12-10 20:51:35 +0900

    読書ログのWEBサービスばんが完成しました
    Bootstrapでスタイルを整え、見栄えがよくなりました。

--- src/views/index.php
+++ src/views/index.php
@@ -1,11 +1,11 @@
-<a href="new.php">読書ログを登録する</a>
+<a href="new.php" class="btn btn-primary mb-4">読書ログを登録する</a>
 <main>
     <?php if (count($reviews) > 0) : ?>
         <?php foreach ($reviews as $review) : ?>
-            <section>
-                <div>
-                    <h2><?php echo escape($review['title']); ?></h2>
-                    <div>
+            <section class="card shadow-sm mb-4">
+                <div class="card-body">
+                    <h2 class="card-title h4 mb-3"><?php echo escape($review['title']); ?></h2>
+                    <div class="small mb-3">
                         <?php echo escape($review['author']); ?>&nbsp;/&nbsp;
                         <?php echo escape($review['status']); ?>&nbsp;/&nbsp;
                         <?php echo escape($review['score']); ?>点
